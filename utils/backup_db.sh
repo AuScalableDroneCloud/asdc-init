58fc2b146eeacf2af1b313fbb7b77d762ddee86d
Owen Kaluza <owen.kaluza@monash.edu>
2022-07-26 18:46:43 +1000

    Add debug check to daily script (runs every minute)

--- utils/backup_db.sh
+++ utils/backup_db.sh
@@ -10,6 +10,20 @@ do
   mkdir -p app/store/backups
   cp --no-preserve=ownership,timestamps webodm_db_latest.dump /webodm/app/store/backups/$fn
   #ln -s /webodm/app/store/backups/$fn /webodm/app/media/webodm_db_latest.dump
-  echo "Sleeping 24 hours..."
-  sleep 86400;
+
+  #echo "Sleeping 24 hours..."
+  #sleep 86400;
+
+  #Do a filesystem access check - to compare output with livenessprobe that is failing
+  #(loops for 1440 minutes = 24 hours)
+  for i in {1..1440}
+  do 
+    echo "[$i] Checking filesystem access... /webodm/app/media"
+    ls /webodm/app/media
+    echo "[$i] Checking filesystem access... /webodm/app/store"
+    ls /webodm/app/store
+    echo "[$i] Sleeping 1 minute"
+    sleep 60;
+  done
+
 done
