90b22179e1823bb6c37ec0abdc2fba9f69971c64
Owen Kaluza <owen.kaluza@monash.edu>
2023-05-12 14:36:45 +1000

    Fix db backup loop, backup every 8 hours
    Removed old filesystem check

--- utils/backup_db.sh
+++ utils/backup_db.sh
@@ -4,30 +4,30 @@
 echo "Removing backups older than 30 days..."
 find /webodm/app/store/backups -type f -mtime +30 -delete
 
-TS=$(date "+%Y%m%d-%H%M")
 echo "Begin backup loop";
 while :;
 do 
   echo "Backing up database..."
+  TS=$(date "+%Y%m%d-%H%M")
   fn=webodm_db_$TS.dump
   pg_dump -h db -U postgres -F c webodm_dev > webodm_db_latest.dump
   mkdir -p app/store/backups
   cp --no-preserve=ownership,timestamps webodm_db_latest.dump /webodm/app/store/backups/$fn
   #ln -s /webodm/app/store/backups/$fn /webodm/app/media/webodm_db_latest.dump
 
-  #echo "Sleeping 24 hours..."
-  #sleep 86400;
+  #echo "Sleeping 8 hours..."
+  sleep 28800;
 
   #Do a filesystem access check - to compare output with livenessprobe that is failing
   #(loops for 1440 minutes = 24 hours)
-  for i in {1..1440}
-  do 
-    echo "[$i] Checking filesystem access... /webodm/app/media"
-    ls /webodm/app/media
-    echo "[$i] Checking filesystem access... /webodm/app/store"
-    ls /webodm/app/store
-    echo "[$i] Sleeping 1 minute"
-    sleep 60;
-  done
+  #for i in {1..1440}
+  #do 
+  #  echo "[$i] Checking filesystem access... /webodm/app/media"
+  #  ls /webodm/app/media
+  #  echo "[$i] Checking filesystem access... /webodm/app/store"
+  #  ls /webodm/app/store
+  #  echo "[$i] Sleeping 1 minute"
+  #  sleep 60;
+  #done
 
 done
